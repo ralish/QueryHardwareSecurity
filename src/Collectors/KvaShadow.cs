14215a3c7897448b466f7119c4fc0a9c9c969ed8
Samuel D. Leslie <sdl@nexiom.net>
2025-12-03 18:07:19 +1100

    Minor improvements to KvaShadow collector

--- src/Collectors/KvaShadow.cs
+++ src/Collectors/KvaShadow.cs
@@ -40,34 +40,46 @@ namespace QueryHardwareSecurity.Collectors {
              * CVE IDs:     CVE-2017-5754
              */
 
-            var kvaShadowEnabled = _kernelVaShadowInfo.KvaShadowEnabled;
-            var kvaShadowUserGlobal = _kernelVaShadowInfo.KvaShadowUserGlobal;
-            var kvaShadowPcid = _kernelVaShadowInfo.KvaShadowPcid;
-            var kvaShadowInvpcid = _kernelVaShadowInfo.KvaShadowInvpcid;
             var kvaShadowRequiredAvailable = _kernelVaShadowInfo.KvaShadowRequiredAvailable;
-            var kvaShadowRequired = kvaShadowRequiredAvailable ? (bool?)_kernelVaShadowInfo.KvaShadowRequired : null;
-
-            /*
-             * The SpeculationControl PowerShell module sets KvaShadowRequired
-             * based on its own logic if KvaShadowRequiredAvailable is false.
-             * That additional logic isn't implemented as it's unclear if it's
-             * really necessary, but for reference the logic used is:
-             *
-             * - For AMD CPUs, sets KvaShadowRequired to false.
-             * - For Intel CPUs, checks the CPU model against a set of values
-             *   and sets it to false if any match (the default is true).
-             * - For other CPUs, throws an exception stating that the processor
-             *   manufacturer is unsupported.
-             */
-            var kvaShadowRequiredSecure = kvaShadowRequired != null && !kvaShadowRequired.Value;
-            var kvaShadowEnabledSecure = kvaShadowEnabled || (kvaShadowRequired != null && !kvaShadowRequired.Value);
-
-            WriteOutputEntry("KvaShadowEnabled", kvaShadowEnabled, kvaShadowEnabledSecure);
-            WriteOutputEntry("KvaShadowUserGlobal", kvaShadowUserGlobal);
-            WriteOutputEntry("KvaShadowPcid", kvaShadowPcid);
-            WriteOutputEntry("KvaShadowInvpcid", kvaShadowInvpcid);
-            WriteOutputEntry("KvaShadowRequired", kvaShadowRequired, kvaShadowRequiredSecure);
             WriteOutputEntry("KvaShadowRequiredAvailable", kvaShadowRequiredAvailable, kvaShadowRequiredAvailable);
+
+            // The SpeculationControl PowerShell module sets KvaShadowRequired
+            // based on its own logic if KvaShadowRequiredAvailable is false.
+            // That additional logic isn't implemented as it's unclear if it's
+            // really necessary, but for reference the logic used is:
+            //
+            // - For AMD CPUs, sets KvaShadowRequired to false.
+            // - For Intel CPUs, checks the CPU model against a set of values
+            //   and sets it to false if any match (the default is true).
+            // - For other CPUs, throws an exception stating that the processor
+            //   manufacturer is unsupported.
+            //
+            // If KvaShadowRequireAvailable isn't set we do override the secure
+            // status for AMD processors as they are never vulnerable.
+            var kvaShadowRequired = kvaShadowRequiredAvailable ? (bool?)_kernelVaShadowInfo.KvaShadowRequired : null;
+            var kvaShadowRequiredSecure = (kvaShadowRequired != null && !kvaShadowRequired.Value) || SystemInfo.IsProcessorAmd;
+            WriteOutputEntry("KvaShadowRequired", kvaShadowRequired, kvaShadowRequiredSecure);
+
+            var kvaShadowEnabled = _kernelVaShadowInfo.KvaShadowEnabled;
+            var kvaShadowEnabledSecure = kvaShadowEnabled || kvaShadowRequiredSecure;
+            WriteOutputEntry("KvaShadowEnabled", kvaShadowEnabled, kvaShadowEnabledSecure);
+
+            var kvaShadowUserGlobal = _kernelVaShadowInfo.KvaShadowUserGlobal;
+            WriteOutputEntry("KvaShadowUserGlobal", kvaShadowUserGlobal);
+
+            var kvaShadowPcid = _kernelVaShadowInfo.KvaShadowPcid;
+            var kvaShadowPcidDescription = "Processor supports ";
+            if (SystemInfo.IsProcessorAmdOrIntel) {
+                kvaShadowPcidDescription += "PCID (AMD / Intel)";
+            } else if (SystemInfo.IsProcessorArm) {
+                kvaShadowPcidDescription += "ASID (ARM)";
+            } else {
+                kvaShadowPcidDescription += "assigning memory pages to processes";
+            }
+            WriteOutputEntry("KvaShadowPcid", kvaShadowPcid, description: kvaShadowPcidDescription);
+
+            var kvaShadowInvpcid = _kernelVaShadowInfo.KvaShadowInvpcid;
+            WriteOutputEntry("KvaShadowInvpcid", kvaShadowInvpcid);
 
             /*
              * L1 Terminal Fault
@@ -77,43 +89,35 @@ namespace QueryHardwareSecurity.Collectors {
              * CVE IDs:     CVE-2018-3620
              */
 
+            // ReSharper disable InconsistentNaming
+
             var l1tfMitigationPresent = _kernelVaShadowInfo.L1TerminalFaultMitigationPresent;
-            var invalidPteBit = l1tfMitigationPresent ? (byte?)_kernelVaShadowInfo.InvalidPteBit : null;
+            // For unpatched systems with a non-Intel processor mark the
+            // status as secure given the vulnerability is not applicable.
+            // ARM systems appear to never set this bit.
+            var l1tfMitigationPresentSecure = l1tfMitigationPresent || !SystemInfo.IsProcessorIntel;
+            WriteOutputEntry("L1TerminalFaultMitigationPresent", l1tfMitigationPresent, l1tfMitigationPresentSecure);
+
+            // ReSharper enable InconsistentNaming
+
             var l1DataCacheFlushSupported = l1tfMitigationPresent ? (bool?)_kernelVaShadowInfo.L1DataCacheFlushSupported : null;
-
-
-            /*
-             * The SpeculationControl PowerShell module determines if the L1TF
-             * mitigation is enabled by checking for any of these conditions:
-             *
-             * - L1TerminalFaultMitigationPresent is true.
-             * - L1DataCacheFlushSupported is true.
-             * - KvaShadowEnabled is true and InvalidPteBit is non-zero.
-             *
-             * This doesn't seem to make much sense as surely the latter two
-             * conditions can't ever be true if the first one isn't met?
-             */
-            var l1tfMitigationEnabled = l1tfMitigationPresent && ((kvaShadowEnabled && invalidPteBit != 0) || l1DataCacheFlushSupported.Value);
-
-            /*
-             * The SpeculationControl PowerShell module implements custom logic
-             * to determine if the L1TF mitigation is required. The logic seems
-             * somewhat redundant and it's not clear if it's necessary so isn't
-             * implemented, but for reference the logic used is:
-             *
-             * - For all non-Intel CPUs, mitigation is not required.
-             * - For Intel CPUs:
-             *   - Mitigation is not required if RdclHardwareProtectedReported
-             *     and RdclHardwareProtected are both true.
-             *   - Mitigation is not required if HvL1tfStatusAvailable and
-             *     HvL1tfProcessorNotAffected are both true.
-             *   - Otherwise, checks the CPU model and stepping against a list
-             *     of values, and sets the mitigation as required if any match.
-             */
-            // TODO: Secure states with above logic & override ARM as secure
-            WriteOutputEntry("InvalidPteBit", invalidPteBit);
             WriteOutputEntry("L1DataCacheFlushSupported", l1DataCacheFlushSupported);
-            WriteOutputEntry("L1TerminalFaultMitigationPresent", l1tfMitigationPresent, l1tfMitigationPresent);
+
+            // The SpeculationControl PowerShell module implements custom logic
+            // to determine if the L1TF mitigation is required. The logic seems
+            // somewhat redundant and it's not clear if it's necessary so isn't
+            // implemented, but for reference the logic used is:
+            //
+            // - For all non-Intel CPUs, mitigation is not required.
+            // - For Intel CPUs:
+            //   - Mitigation is not required if RdclHardwareProtectedReported
+            //     and RdclHardwareProtected are both true.
+            //   - Mitigation is not required if HvL1tfStatusAvailable and
+            //     HvL1tfProcessorNotAffected are both true.
+            //   - Otherwise, checks the CPU model and stepping against a list
+            //     of values, and sets the mitigation as required if any match.
+            var invalidPteBit = l1tfMitigationPresent ? (byte?)_kernelVaShadowInfo.InvalidPteBit : null;
+            WriteOutputEntry("InvalidPteBit", invalidPteBit);
         }
 
         #region P/Invoke
