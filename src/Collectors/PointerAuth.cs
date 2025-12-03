3466d459f37b43c799d07353378a7579e4eadbe2
Samuel D. Leslie <sdl@nexiom.net>
2025-12-03 16:54:29 +1100

    Fix PointerAuth collector on .NET runtimes (non-Framework)

--- src/Collectors/PointerAuth.cs
+++ src/Collectors/PointerAuth.cs
@@ -21,6 +21,7 @@ namespace QueryHardwareSecurity.Collectors {
 
             var ntStatus = NtQuerySystemInformation(PointerAuthInfoClass, out _pointerAuthInfo, (uint)pointerAuthInfoLength, IntPtr.Zero);
             if (ntStatus != 0) NtQsiFailure(ntStatus);
+            WriteDebug($"Result: 0x{_pointerAuthInfo._RawBits:X8}");
         }
 
         internal override string ConvertToJson() {
@@ -31,15 +32,11 @@ namespace QueryHardwareSecurity.Collectors {
             SetOutputSettings(format, color);
             WriteOutputHeader();
 
-            // Displaying the symbolic names of the SupportedFlags flags will
-            // make the value column too wide in table format, so we'll display
-            // the flags as individual booleans.
-            var supportedFlags = _pointerAuthInfo.SupportedFlags;
-            var addressAuthSupported = supportedFlags.HasFlag(SupportedFlags.AddressAuthSupported);
-            var addressAuthQarma = supportedFlags.HasFlag(SupportedFlags.AddressAuthQarma);
-            var genericAuthSupported = supportedFlags.HasFlag(SupportedFlags.GenericAuthSupported);
-            var genericAuthQarma = supportedFlags.HasFlag(SupportedFlags.GenericAuthQarma);
-            var addressAuthFaulting = supportedFlags.HasFlag(SupportedFlags.AddressAuthFaulting);
+            var addressAuthSupported = _pointerAuthInfo.AddressAuthSupported;
+            var addressAuthQarma = _pointerAuthInfo.AddressAuthQarma;
+            var genericAuthSupported = _pointerAuthInfo.GenericAuthSupported;
+            var genericAuthQarma = _pointerAuthInfo.GenericAuthQarma;
+            var addressAuthFaulting = _pointerAuthInfo.AddressAuthFaulting;
 
             WriteOutputEntry("AddressAuthSupported", addressAuthSupported, addressAuthSupported);
             WriteOutputEntry("AddressAuthQarma", addressAuthQarma, addressAuthQarma);
@@ -47,10 +44,9 @@ namespace QueryHardwareSecurity.Collectors {
             WriteOutputEntry("GenericAuthQarma", genericAuthQarma, genericAuthQarma);
             WriteOutputEntry("AddressAuthFaulting", addressAuthFaulting, addressAuthFaulting);
 
-            var enabledFlags = _pointerAuthInfo.EnabledFlags;
-            var userPerProcessIpAuthEnabled = enabledFlags.UserPerProcessIpAuthEnabled;
-            var userGlobalIpAuthEnabled = enabledFlags.UserGlobalIpAuthEnabled;
-            var kernelIpAuthEnabled = enabledFlags.KernelIpAuthEnabled;
+            var userPerProcessIpAuthEnabled = _pointerAuthInfo.UserPerProcessIpAuthEnabled;
+            var userGlobalIpAuthEnabled = _pointerAuthInfo.UserGlobalIpAuthEnabled;
+            var kernelIpAuthEnabled = _pointerAuthInfo.KernelIpAuthEnabled;
 
             var userPerProcessIpAuthEnabledSecure = userPerProcessIpAuthEnabled || userGlobalIpAuthEnabled;
 
@@ -67,39 +63,32 @@ namespace QueryHardwareSecurity.Collectors {
                                                            uint systemInformationLength,
                                                            IntPtr returnLength);
 
-#pragma warning disable CS0649 // Field is never assigned to
-
-        private struct PointerAuthInfo {
-            internal SupportedFlags SupportedFlags;
-            internal EnabledFlags EnabledFlags;
-        }
-
-        // @formatter:int_align_fields true
-
-        [Flags]
-        private enum SupportedFlags : ushort {
-            AddressAuthSupported = 0x1,
-            AddressAuthQarma     = 0x2,
-            GenericAuthSupported = 0x4,
-            GenericAuthQarma     = 0x8,
-            AddressAuthFaulting  = 0x10
-        }
-
-        // @formatter:int_align_fields false
-
+#pragma warning disable CS0649  // Field is never assigned to
 #pragma warning disable IDE0044 // Field can be made readonly
 #pragma warning disable IDE0251 // Member can be made readonly
 
         // ReSharper disable InconsistentNaming
 
-        private struct EnabledFlags {
-            private ushort _RawBits;
+        private struct PointerAuthInfo {
+            internal uint _RawBits;
 
-            public bool UserPerProcessIpAuthEnabled => (_RawBits & 0x1) == 1;    // Bit 0
-            public bool UserGlobalIpAuthEnabled => ((_RawBits >> 1) & 0x1) == 1; // Bit 1
-            private byte UserEnabledReserved => (byte)((_RawBits >> 2) & 0x3F);  // Bits 2-7
-            public bool KernelIpAuthEnabled => ((_RawBits >> 8) & 0x1) == 1;     // Bit 8
-            private byte KernelEnabledReserved => (byte)(_RawBits >> 9);         // Bits 9-15
+            /*
+             * SupportedFlags
+             */
+            public bool AddressAuthSupported => (_RawBits & 0x1) == 1;        // Bit 0
+            public bool AddressAuthQarma => ((_RawBits >> 1) & 0x1) == 1;     // Bit 1
+            public bool GenericAuthSupported => ((_RawBits >> 2) & 0x1) == 1; // Bit 2
+            public bool GenericAuthQarma => ((_RawBits >> 3) & 0x1) == 1;     // Bit 3
+            public bool AddressAuthFaulting => ((_RawBits >> 4) & 0x1) == 1;  // Bit 4
+
+            /*
+             * EnabledFlags
+             */
+            public bool UserPerProcessIpAuthEnabled => ((_RawBits >> 16) & 0x1) == 1; // Bit 16
+            public bool UserGlobalIpAuthEnabled => ((_RawBits >> 17) & 0x1) == 1;     // Bit 17
+            private byte UserEnabledReserved => (byte)((_RawBits >> 18) & 0x3F);      // Bits 18-23
+            public bool KernelIpAuthEnabled => ((_RawBits >> 24) & 0x1) == 1;         // Bit 24
+            private byte KernelEnabledReserved => (byte)(_RawBits >> 9);              // Bits 25-31
         }
 
         // ReSharper restore InconsistentNaming
