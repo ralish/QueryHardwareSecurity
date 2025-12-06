e9ab7e9780f7a6c599030866d2d869e5837c0d3c
Samuel D. Leslie <sdl@nexiom.net>
2025-12-07 02:10:49 +1100

    Major improvements to Ium collector
    Notable changes:
    - Added new SecretsTpmBound field and description
    - Only output HVPT fields on supported Windows releases
    - All field descriptions are now documented
    - Added several TODO items for future

--- src/Collectors/Ium.cs
+++ src/Collectors/Ium.cs
@@ -16,12 +16,19 @@ namespace QueryHardwareSecurity.Collectors {
         private void RetrieveInfo() {
             WriteVerbose($"Retrieving {Name} info ...");
 
+            // For the TrustletRunning flag to be populated we need to use
+            // NtQuerySystemInformationEx and provide an 8 byte input buffer
+            // with the first bit set. In practice this is most likely a bit
+            // field but it's completely undocumented. As of Windows 11 25H2
+            // there are no other bits which affect the returned results.
+            ulong iumInput = 1;
+
             var iumInfoLength = Marshal.SizeOf<IsolatedUserModeInfo>();
             WriteDebug($"Size of {nameof(IsolatedUserModeInfo)} structure: {iumInfoLength} bytes");
 
-            var ntStatus = NtQuerySystemInformation(IsolatedUserModeInfoClass, out _iumInfo, (uint)iumInfoLength, IntPtr.Zero);
+            var ntStatus = NtQuerySystemInformationEx(IsolatedUserModeInfoClass, ref iumInput, sizeof(ulong), out _iumInfo, (uint)iumInfoLength, IntPtr.Zero);
             if (ntStatus != 0) NtQsiFailure(ntStatus);
-            WriteDebug($"Result: 0x{_iumInfo._RawBits:X4}");
+            WriteDebug($"Result: 0x{_iumInfo._RawBits:X8}");
         }
 
         internal override string ConvertToJson() {
@@ -32,42 +39,68 @@ namespace QueryHardwareSecurity.Collectors {
             SetOutputSettings(format, color);
             WriteOutputHeader();
 
-            var secureKernelRunning = _iumInfo.SecureKernelRunning;
-            var hvciEnabled = _iumInfo.HvciEnabled;
-            var hvciStrictMode = _iumInfo.HvciStrictMode;
-            var debugEnabled = _iumInfo.DebugEnabled;
             var firmwarePageProtection = _iumInfo.FirmwarePageProtection;
-            var encryptionKeyAvailable = _iumInfo.EncryptionKeyAvailable;
-            var trustletRunning = _iumInfo.TrustletRunning;
-            var hvciDisableAllowed = _iumInfo.HvciDisableAllowed;
-            var hardwareEnforcedVbs = _iumInfo.HardwareEnforcedVbs;
-            var noSecrets = _iumInfo.NoSecrets;
-            var encryptionKeyPersistent = _iumInfo.EncryptionKeyPersistent;
-            var hardwareEnforcedHvpt = _iumInfo.HardwareEnforcedHvpt;
-            var hardwareHvptAvailable = _iumInfo.HardwareHvptAvailable;
-
-            WriteOutputEntry("SecureKernelRunning", secureKernelRunning, secureKernelRunning);
-            WriteOutputEntry("HvciEnabled", hvciEnabled, hvciEnabled);
-            WriteOutputEntry("HvciStrictMode", hvciStrictMode, hvciStrictMode);
-            WriteOutputEntry("DebugEnabled", debugEnabled, !debugEnabled);
             WriteOutputEntry("FirmwarePageProtection", firmwarePageProtection, firmwarePageProtection);
-            WriteOutputEntry("EncryptionKeyAvailable", encryptionKeyAvailable, encryptionKeyAvailable);
-            WriteOutputEntry("TrustletRunning", trustletRunning);
-            WriteOutputEntry("HvciDisableAllowed", hvciDisableAllowed, !hvciDisableAllowed);
+
+            var secureKernelRunning = _iumInfo.SecureKernelRunning;
+            WriteOutputEntry("SecureKernelRunning", secureKernelRunning, secureKernelRunning);
+
+            // TODO: Determine where this is set
+            var hardwareEnforcedVbs = _iumInfo.HardwareEnforcedVbs;
             WriteOutputEntry("HardwareEnforcedVbs", hardwareEnforcedVbs, hardwareEnforcedVbs);
-            WriteOutputEntry("NoSecrets", noSecrets);
+
+            var encryptionKeyAvailable = _iumInfo.EncryptionKeyAvailable;
+            WriteOutputEntry("EncryptionKeyAvailable", encryptionKeyAvailable, encryptionKeyAvailable);
+
+            var encryptionKeyPersistent = _iumInfo.EncryptionKeyPersistent;
             WriteOutputEntry("EncryptionKeyPersistent", encryptionKeyPersistent, encryptionKeyPersistent);
-            WriteOutputEntry("HardwareEnforcedHvpt", hardwareEnforcedHvpt, hardwareEnforcedHvpt);
-            WriteOutputEntry("HardwareHvptAvailable", hardwareHvptAvailable, hardwareHvptAvailable);
+
+            var noSecrets = _iumInfo.NoSecrets;
+            WriteOutputEntry("NoSecrets", noSecrets, !noSecrets);
+
+            // Undocumented (inc. in Process Hacker NT headers)
+            // TODO: Determine Windows version requirement
+            var secretsTpmBound = _iumInfo.SecretsTpmBound;
+            WriteOutputEntry("SecretsTpmBound", secretsTpmBound, secretsTpmBound);
+
+            var hvciEnabled = _iumInfo.HvciEnabled;
+            WriteOutputEntry("HvciEnabled", hvciEnabled, hvciEnabled);
+
+            var hvciDisableAllowed = _iumInfo.HvciDisableAllowed;
+            WriteOutputEntry("HvciDisableAllowed", hvciDisableAllowed, !hvciDisableAllowed);
+
+            // TODO: N/A if HvciEnabled is false?
+            var hvciStrictMode = _iumInfo.HvciStrictMode;
+            WriteOutputEntry("HvciStrictMode", hvciStrictMode, hvciStrictMode);
+
+            // TODO: N/A if HvciEnabled is false?
+            var debugEnabled = _iumInfo.DebugEnabled;
+            WriteOutputEntry("DebugEnabled", debugEnabled, !debugEnabled);
+
+            // HVPT is only supported on recent Intel processors
+            var hardwareHvptAvailable = _iumInfo.HardwareHvptAvailable;
+            var hardwareHvptAvailableSecure = hardwareHvptAvailable || !SystemInfo.IsProcessorIntel;
+            WriteOutputEntry("HardwareHvptAvailable", hardwareHvptAvailable, hardwareHvptAvailableSecure);
+
+            // HVPT is only supported on recent Intel processors
+            var hardwareEnforcedHvpt = _iumInfo.HardwareEnforcedHvpt;
+            var hardwareEnforcedHvptSecure = (hardwareHvptAvailable && hardwareEnforcedHvpt) || !SystemInfo.IsProcessorIntel;
+            WriteOutputEntry("HardwareEnforcedHvpt", hardwareEnforcedHvpt, hardwareEnforcedHvptSecure);
+
+            // TODO: Display Credential Guard and Key Guard status
+            var trustletRunning = _iumInfo.TrustletRunning;
+            WriteOutputEntry("TrustletRunning", trustletRunning, trustletRunning);
         }
 
         #region P/Invoke
 
         [DllImport("ntdll", ExactSpelling = true)]
-        private static extern int NtQuerySystemInformation(int systemInformationClass,
-                                                           out IsolatedUserModeInfo systemInformation,
-                                                           uint systemInformationLength,
-                                                           IntPtr returnLength);
+        private static extern int NtQuerySystemInformationEx(int systemInformationClass,
+                                                             ref ulong inputBuffer,
+                                                             uint inputBufferLength,
+                                                             out IsolatedUserModeInfo systemInformation,
+                                                             uint systemInformationLength,
+                                                             IntPtr returnLength);
 
 #pragma warning disable CA1823  // Avoid unused private fields
 #pragma warning disable CS0169  // Field is never used
@@ -78,7 +111,7 @@ namespace QueryHardwareSecurity.Collectors {
         // ReSharper disable InconsistentNaming
 
         private struct IsolatedUserModeInfo {
-            internal ushort _RawBits;
+            internal uint _RawBits;
 
             public bool SecureKernelRunning => (_RawBits & 0x1) == 1;             // Bit 0
             public bool HvciEnabled => ((_RawBits >> 1) & 0x1) == 1;              // Bit 1
@@ -95,10 +128,9 @@ namespace QueryHardwareSecurity.Collectors {
             public bool HardwareEnforcedHvpt => ((_RawBits >> 13) & 0x1) == 1;    // Bit 13
             public bool HardwareHvptAvailable => ((_RawBits >> 14) & 0x1) == 1;   // Bit 14
             private bool SpareFlags2 => ((_RawBits >> 15) & 0x1) == 1;            // Bit 15
+            public bool SecretsTpmBound => ((_RawBits >> 16) & 0x1) == 1;         // Bit 16
 
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = 6)]
-            private bool[] Spare0; // Bits 16-63
-
+            private uint Spare0;  // Bits 32-63
             private ulong Spare1; // Bits 64-127
         }
 
